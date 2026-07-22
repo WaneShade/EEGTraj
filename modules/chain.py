cc874f1e6421d02789824acb6a74090957ed53c2
FallenShadow <107897993+WaneShade@users.noreply.github.com>
2026-07-22 13:59:42 +0800

    Delete modules directory

--- modules/chain.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility exports for the current shared-chain schema."""
-
-from modules.chain_schema import *  # noqa: F401,F403
--- modules/trainers.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Public training entry points."""
-
-from modules.grpo_core import main as grpo_main
-from modules.sft_runner import main as sft_main
-
-
-__all__ = ["grpo_main", "sft_main"]
